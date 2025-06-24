c16e31c17ec122eb6c90dba327cf739a24629cd7
Yohen Sheraun <ysheraun20@yahoo.com>
2025-06-24 19:19:06 +0800

    feat: Added double newline check

--- srcs/map_extractor.c
+++ srcs/map_extractor.c
@@ -6,11 +6,24 @@
 /*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 17:40:33 by yosherau          #+#    #+#             */
-/*   Updated: 2025/04/11 15:34:04 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/06/24 19:18:12 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
+
+void	check_double_newline(char *one_d_map)
+{
+	int	index;
+
+	index = -1;
+	while (one_d_map[++index])
+	{
+		if (one_d_map[index] == '\n' && one_d_map[index + 1] == '\n')
+			exit(1);
+	}
+	return ;
+}
 
 char	**map_extractor(char *map)
 {
@@ -33,6 +46,7 @@ char	**map_extractor(char *map)
 		free(temp);
 		line = get_next_line(fd);
 	}
+	check_double_newline(one_d_map);
 	two_d_map = ft_split(one_d_map, '\n');
 	free(one_d_map);
 	return (two_d_map);
